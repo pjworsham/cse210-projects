2cfc622d3ddfdec55b7015582aaa93e46c8647c3
pjworsham <worsham.pj@gmail.com>
2024-04-27 13:49:21 -0400

    Input grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        string letter;
+
+        if (number >=90)
+        {
+           letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+
+       if (number >= 70)
+       {
+            Console.WriteLine("Congratulations, you passed the class! ");
+       }
+       else
+       {
+            Console.WriteLine("You did not pass the class. Try again later!");
+       }
+
     }
 }
