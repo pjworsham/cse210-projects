484161af6491bc94b37dd3a27fdb34b6202264d6
pjworsham <worsham.pj@gmail.com>
2024-05-23 23:13:00 -0400

    Eternal Quest Program start

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -5,16 +5,13 @@ public class GoalManager
     private List<Goal> _goals;
     private int _score;
 
-    public GoalManager(string name, string description, int points)
+    public GoalManager() 
     {
         _goals = new List<Goal>(); 
         _score = 0;
 
     }
-    public GoalManager()
-    {
-
-    }
+  
     public void Start()
     {
 
